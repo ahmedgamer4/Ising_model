7274641cb014f6543ea4058575710b613b7c51e8
ahmedgamer4 <aa10@pme.suezuni.edu.eg>
2022-08-26 21:57:05 +0200

    Not done

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from distutils.command.config import config
-from tkinter import N
 import numpy as np
 import matplotlib as plt
 from numpy.random import rand
@@ -7,7 +5,16 @@ from scipy.sparse import spdiags, linalg, eye
 
 # Functions that will be used.
 
-def initialstate(N):
+N = 10              # Size of the lattice
+n_t = 32            # Number of temperature points
+eq_steps = 2**8     # Number of MC sweeps fo equilibration
+mc_steps = 2**10    # Number of MC sweeps for calculation
+
+T       = np.linspace(1.53, 3.28, n_t); 
+E,M,C,X = np.zeros(n_t), np.zeros(n_t), np.zeros(n_t), np.zeros(n_t)
+n1, n2  = 1.0/(mc_steps*N*N), 1.0/(mc_steps*mc_steps*N*N) 
+
+def initialstate():
     # Generate a random spin config for initial condition
     state = 2*np.random.randint(2, size=(N,N)) - 1
     return state
@@ -31,7 +38,7 @@ def mcmove(config, beta):
     return config
 
 def calc_energy(config):
-    
+    energy = 0
     for i in range(len(config)):
         for j in range(len(config)):
             S = config[i, j]
@@ -47,11 +54,60 @@ def calc_mag(config):
     return mag
 
 
-N = 10              # Size of the lattice
-n_t = 32            # Number of temperature points
-eq_steps = 2**8     # Number of MC sweeps fo equilibration
-mc_steps = 2**10    # Number of MC sweeps for calculation
+# Main
 
-T       = np.linspace(1.53, 3.28, nt); 
-E,M,C,X = np.zeros(n_t), np.zeros(n_t), np.zeros(n_t), np.zeros(n_t)
-n1, n2  = 1.0/(mc_steps*N*N), 1.0/(mc_steps*mc_steps*N*N) 
+for tt in range(n_t):
+    config = initialstate()
+    
+    E1 = M1 = E2 = M2 = 0
+    iT = 1.0/T[tt]
+    iT2 = iT * iT
+    
+    for i in range(eq_steps):
+        mcmove(config, iT)
+        
+    for i in range(mc_steps):
+        mcmove(config, iT)
+        ene = calc_energy(config)
+        mag = calc_mag(config)
+        
+        E1 = E1 + ene
+        M2 = M2 + mag
+        E2 = E2 + ene*ene
+        M1 = M1 + mag*mag
+        
+        
+    E[tt] = n1*E1
+    M[tt] = n1*M1
+    C[tt] = (n1*E2 - n2*E1*E1)*iT2
+    X[tt] = (n1*M2 - n2*M1*M1)*iT
+    
+f = plt.figure(figsize=(18, 10)); #  
+
+
+sp =  f.add_subplot(2, 2, 1 )
+plt.scatter(T, E, s=50, marker='o', color='IndianRed')
+plt.xlabel("Temperature (T)", fontsize=20);
+plt.ylabel("Energy ", fontsize=20)      
+plt.axis('tight')
+
+
+sp =  f.add_subplot(2, 2, 2 );
+plt.scatter(T, abs(M), s=50, marker='o', color='RoyalBlue')
+plt.xlabel("Temperature (T)", fontsize=20)
+plt.ylabel("Magnetization ", fontsize=20)  
+plt.axis('tight')
+
+
+sp =  f.add_subplot(2, 2, 3 );
+plt.scatter(T, C, s=50, marker='o', color='IndianRed')
+plt.xlabel("Temperature (T)", fontsize=20)  
+plt.ylabel("Specific Heat ", fontsize=20)
+plt.axis('tight') 
+
+
+sp =  f.add_subplot(2, 2, 4 )
+plt.scatter(T, X, s=50, marker='o', color='RoyalBlue')
+plt.xlabel("Temperature (T)", fontsize=20) 
+plt.ylabel("Susceptibility", fontsize=20)
+plt.axis('tight')
